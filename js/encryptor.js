4a942b1b568a21b9dcaf13a3be5252f04bd2e5b2
Ronald <dada5714@yahoo.com>
2014-10-22 09:42:35 -0700

    Initial commit

---
+++ js/encryptor.js
@@ -0,0 +1,27 @@
+    window.resize = console.log("Hello World");
+      var encoder = {
+        setMessage: function() {
+          this.message = document.getElementById('message').value
+        },
+        getPassword: function() {
+          this.password = prompt("Enter Password")
+        },
+        setUrl: function() {
+          this.url = this.encrypt()
+        },
+        replaceMessageWithUrl: function () {
+          document.getElementById('message').value = "file:///home/rock/rails_projects/hearsay/decoder.html?" + this.url
+        },
+        encrypt: function() {
+          cypher = CryptoJS.AES.encrypt(this.message, this.password)
+          console.log("Verified: " +
+            CryptoJS.AES.decrypt(cypher, this.password).toString(CryptoJS.enc.Utf8))
+          return cypher
+        },
+        protect: function() {
+          this.setMessage()
+          this.getPassword()
+          this.setUrl()
+          this.replaceMessageWithUrl()
+        }
+      }
